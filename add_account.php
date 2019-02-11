3d99c759222a8563702dcb42f6641d9490296ad5
pgueruela <pgueruela@gmail.com>
2019-02-11 16:56:27 +0800

    column in field has been edited

--- add_account.php
+++ add_account.php
@@ -14,7 +14,7 @@ include 'process/add_account_process.php';
 	<div class="container">
 		 <div class="form-group">
 		 		<div class="row">
-					<div class="col-md-4">
+					<div class="col-md-8">
 						<label for="exampleInputEmail1">Firstname</label>
 		    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="firstname" required/>
 			
@@ -22,7 +22,7 @@ include 'process/add_account_process.php';
 		  </div>
 		  <div class="form-group">
 		 		<div class="row">
-					<div class="col-md-4">
+					<div class="col-md-8">
 						<label for="exampleInputEmail1">Lastname</label>
 		    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="lastname" required/>
 					</div>
@@ -30,7 +30,7 @@ include 'process/add_account_process.php';
 		  </div>
 		  <div class="form-group">
 		 		<div class="row">
-					<div class="col-md-4">
+					<div class="col-md-8">
 						 <label for="exampleFormControlSelect1">Usertype</label>
 						    <select class="form-control" id="exampleFormControlSelect1" name="usertype">
 						      <option>Nurse</option>
@@ -41,7 +41,7 @@ include 'process/add_account_process.php';
 		  </div>
 		<div class="form-group">
 			<div class="row">
-				<div class="col-md-4">
+				<div class="col-md-8">
 					<label for="exampleFormControlSelect1">Username</label>
 		    		<input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="username" required/>
 				</div>
@@ -50,7 +50,7 @@ include 'process/add_account_process.php';
 
 		  <div class="form-group">
 		 		<div class="row">
-					<div class="col-md-4">
+					<div class="col-md-8">
 						<label for="exampleInputEmail1">Password</label>
 		    <input type="password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="password" required/>	
 					</div>
@@ -58,7 +58,7 @@ include 'process/add_account_process.php';
 		 </div>
 		  <div class="form-group">
 		 		<div class="row">
-					<div class="col-md-4">
+					<div class="col-md-8">
 						<label for="exampleInputEmail1">Confirm Password</label>
 		    <input type="password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="confirm_password" required/>
 
@@ -67,7 +67,7 @@ include 'process/add_account_process.php';
 		  </div>
 
 		  <div class="row">
-		  	<div class="col-md-4">
+		  	<div class="col-md-8">
 		  		<button type="submit" class="btn btn-primary" name="register">Register</button>
 		  	</div>
 		  </div>
